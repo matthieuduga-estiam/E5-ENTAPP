58c6a690ecc3443a5dd65a5fa58266b09f3843a3
Matthieu DUGA <matthieu.duga@gmail.com>
2023-11-21 15:30:17 +0100

    Base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './img/estiam.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" width={450} height={130}/>
+        <h1>Matthieu</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
