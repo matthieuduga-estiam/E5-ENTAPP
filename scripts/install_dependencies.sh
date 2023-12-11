536e68ba28076e47af6a559ac54f05ba51a417c6
Matthieu DUGA <matthieu.duga@gmail.com>
2023-12-11 16:17:02 +0100

    AAA

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -25,7 +25,7 @@ fi
 # we need to traverse to where the application bundle is copied too.
 echo installing application with npm
 cd /var/www/
-sudo npm install
+npm install
 
 echo installing pm2
-sudo npm install pm2 -g
+npm install pm2 -g
