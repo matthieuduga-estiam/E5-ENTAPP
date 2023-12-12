abecd07eab5fc49479d6f5f2c47c17955bbe2dc5
Matthieu DUGA <56045108+matthieuduga@users.noreply.github.com>
2023-12-12 19:21:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width={450} height={130}/>
         <h1>Matthieu</h1>
+        <h6>{process.env.REACT_APP_API_URL}</h6>
       </header>
     </div>
   );
